85e3d2cd57f58878d851ae0675babb56c82c4c64
prashant <prashantramangupta@gmail.com>
2020-02-26 15:41:46 +0530

    stake holder data for logged in user along  with stake window data

--- staking/infrastructure/repositories/stake_holder_repository.py
+++ staking/infrastructure/repositories/stake_holder_repository.py
@@ -1,6 +1,7 @@
 from staking.infrastructure.repositories.base_repository import BaseRepository
 from staking.infrastructure.models import StakeHolder
 from staking.domain.factory.stake_factory import StakeFactory
+from sqlalchemy import func
 
 
 class StakeHolderRepository(BaseRepository):
@@ -18,3 +19,13 @@ class StakeHolderRepository(BaseRepository):
                          in stake_holder_raw_data]
         self.session.commit()
         return stake_holders
+
+    def get_total_no_of_stakers(self, blockchain_id):
+        total_no_of_stakers = self.session.query(StakeHolder).filter(StakeHolder.blockchain_id == blockchain_id).count()
+        return total_no_of_stakers
+
+    def get_total_stake_deposited(self, blockchain_id):
+        total_stake_deposited = self.session.query(
+            func.sum(StakeHolder.amount_pending_for_approval).label("total_stake_deposited")).filter(
+            StakeHolder.blockchain_id == blockchain_id).all()
+        return int(total_stake_deposited[0].total_stake_deposited)
