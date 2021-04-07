f595ec270356c152fa88aa5894b33c5e26d74273
prashant <prashantramangupta@gmail.com>
2021-04-07 13:16:44 +0530

    add missing method in repository

--- staking/infrastructure/repositories/stake_holder_repository.py
+++ staking/infrastructure/repositories/stake_holder_repository.py
@@ -24,3 +24,26 @@ class StakeHolderRepository(BaseRepository):
             staker_balance = StakeFactory.convert_stake_holder_db_model_to_entity_model(staker_balance_db)
         return staker_balance
 
+    def add_or_update_stake_holder(self, stake_holder):
+        staker = stake_holder.staker
+        try:
+            stake_holder_db = self.session.query(StakeHolderDBModel).filter(StakeHolderDBModel.staker == staker).first()
+            if stake_holder_db:
+                stake_holder_db.amount_pending_for_approval = stake_holder.amount_pending_for_approval
+                stake_holder_db.amount_approved = stake_holder.amount_approved
+                stake_holder_db.block_no_created = stake_holder.block_no_created
+            self.session.commit()
+        except Exception as e:
+            self.session.rollback()
+            raise e
+        if not stake_holder_db:
+            self.add_item(StakeHolderDBModel(
+                staker=staker,
+                amount_pending_for_approval=stake_holder.amount_pending_for_approval,
+                amount_approved=stake_holder.amount_approved,
+                block_no_created=stake_holder.block_no_created,
+                created_on=dt.utcnow(),
+                updated_on=dt.utcnow()
+            ))
+        return stake_holder
+
